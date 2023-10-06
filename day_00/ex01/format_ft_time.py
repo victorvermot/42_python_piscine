ed83b562042ba1243a66449d7c50c12c957e84d5
victorvermot <victorvermot@gmail.com>
2023-10-06 14:18:43 +0200

    finished ex01 and started ex02

--- day_00/ex01/format_ft_time.py
+++ day_00/ex01/format_ft_time.py
@@ -3,8 +3,10 @@ from datetime import time, datetime
 def main():
     now = datetime.now()
     back = datetime.fromtimestamp(0)
-    print("Seconds since January 1, 1970: " "{:,.4f}".format((now-back).total_seconds()))
-    print(f"{now.month}")
+    seconds_left = (now - back).total_seconds()
+
+    print("Seconds since", "{}:".format(back.strftime("%B %-d, %Y")), "{:,.4f}".format(seconds_left), "or {:.2e}".format(seconds_left), "in scientific notation")
+    print("{}".format(now.strftime("%b %-d %Y")))
 
 if __name__ == "__main__":
     main()
---
+++ day_00/ex02/find_ft_type.py
@@ -0,0 +1,17 @@
+def all_thing_is_obj(object: any) -> int:
+    print(type(object))
+
+def main():
+    ft_list = ["Hello", "tata!"]
+    ft_tuple = ("Hello", "toto!")
+    ft_set = {"Hello", "tutu!"}
+    ft_dict = {"Hello" : "titi!"}
+    all_thing_is_obj(ft_list)
+    all_thing_is_obj(ft_tuple)
+    all_thing_is_obj(ft_set)
+    all_thing_is_obj(ft_dict)
+    all_thing_is_obj("Brian")
+    print(all_thing_is_obj(10))
+
+if __name__ == "__main__":
+    main()
