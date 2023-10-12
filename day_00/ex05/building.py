1472083fb431a81a86ff12322e00c98a4d3622be
Victor Vermot-Petit-Outhenin <victorvermot@gmail.com>
2023-10-12 16:34:30 +0200

    finished ex05

--- day_00/ex05/building.py
+++ day_00/ex05/building.py
@@ -1,40 +1,32 @@
 import sys
-import string
 sys.tracebacklimit = 0
 
 def count_characters(s: str) -> dict:
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     res = {
-        "upper_letters": 0,
-        "lower_letters": 0,
-        "punctuations": 0,
-        "spaces": 0,
-        "digits": 0,
+        "upper_letters": [str.isupper, 0],
+        "lower_letters": [str.islower, 0],
+        "punctuations": [lambda f: f in punctuations, 0],
+        "spaces": [str.isspace, 0],
+        "digits": [str.isdigit, 0],
     }
-    for c in s:n
-        if c.isupper():
-            res["upper_letters"] += 1
-        elif c.islower():
-            res["lower_letters"] += 1
-        elif c.isdigit():
-            res["digits"] += 1
-        elif c.isspace():
-            res["spaces"] += 1
-        elif c in punctuations:
-            res["punctuations"] += 1
+    for letter in s:
+        for x in res.values():
+            if x[0](letter):
+                x[1] += 1
     return res
         
 
 def main():
     if len(sys.argv) != 2:
-        raise AssertionError("more than one argument is provided")
+        raise AssertionError("Please provide one argument only")
     res = count_characters(sys.argv[1])
     print(f"The text contains {len(sys.argv[1])} characters:")
-    print(f"{res['upper_letters']} upper letters")
-    print(f"{res['lower_letters']} lower letters")
-    print(f"{res['punctuations']} punctuation marks")
-    print(f"{res['spaces']} spaces")
-    print(f"{res['digits']} digits")
+    print(f"{res['upper_letters'][1]} upper letters")
+    print(f"{res['lower_letters'][1]} lower letters")
+    print(f"{res['punctuations'][1]} punctuation marks")
+    print(f"{res['spaces'][1]} spaces")
+    print(f"{res['digits'][1]} digits")
 
 if __name__ == "__main__":
     main()
