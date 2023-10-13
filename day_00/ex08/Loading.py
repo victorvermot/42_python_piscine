ddbab0c38bfd6b92fca8b53a1a71e35d87720293
Victor Vermot-Petit-Outhenin <victorvermot@gmail.com>
2023-10-13 16:13:15 +0200

    add ex08 and 09

--- day_00/ex08/Loading.py
+++ day_00/ex08/Loading.py
@@ -1,6 +1,14 @@
 def ft_tqdm(lst: range) -> None:
     count = 0
+    bar_length = 105
+    total = len(lst)
+
     for i in lst:
-        print("=", end="")
-        yield count
         count += 1
+        yield
+        filled_up_length = int(round(bar_length * count / float(total)))
+        percentage = round(100.0 * count/float(total), 1)
+        bar = '=' * filled_up_length + ' ' * (bar_length - filled_up_length)
+        if count == total:
+            bar = bar + '>'
+        print(f"\r{int(percentage)}%|[{bar}]| {count}/{len(lst)}", end="")
---
+++ day_00/ex09/src/ft_package/utils.py
@@ -0,0 +1,2 @@
+def count_in_list(list: list, elem):
+    return list.count(elem)
