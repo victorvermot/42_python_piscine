de202a5ba7dad5069ab0c4721759080352913448
Victor Vermot-Petit-Outhenin <victorvermot@gmail.com>
2023-10-17 17:32:45 +0200

    added ex02

--- day_01/ex02/load_image.py
+++ day_01/ex02/load_image.py
@@ -1,5 +1,23 @@
-
+from imageio import read
+from numpy import array
+from pathlib import Path
 
 
 def ft_load(path: str) -> array:
-    
+    exstention_accepted = [".jpg", ".jpeg", ".png"]
+    file_extension = Path(path).suffix
+    if file_extension not in exstention_accepted:
+        raise AssertionError("File has wrong format")
+    try:
+        img = read(path)
+    except FileNotFoundError:
+        raise AssertionError("The file could not be found") from None
+    res = array(img.get_data(0))
+    print(f"""The shape of the image is:(
+          {len(res)},
+          {len(res[0])},
+          {len(res[0][0])}
+        )
+        """)
+    img.close()
+    return res
